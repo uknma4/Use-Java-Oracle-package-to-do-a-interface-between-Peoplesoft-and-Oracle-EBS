dc5ad33be64c6854a1b285e0b3f97fe5abc56385
Eddie <ukn_ma4@126.com>
2025-09-05 15:32:43 +0800

    Update GetDIDIdata.java

--- classes/GetDIDIdata.java
+++ classes/GetDIDIdata.java
@@ -42,24 +42,7 @@ public class GetDIDIdata {
 
 	public static String getSha1(String str) {
 
-		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-				'a', 'b', 'c', 'd', 'e', 'f' };
-		try {
-			MessageDigest mdTemp = MessageDigest.getInstance("SHA1");
-			mdTemp.update(str.getBytes("UTF-8"));
-			byte[] md = mdTemp.digest();
-			int j = md.length;
-			char buf[] = new char[j * 2];
-			int k = 0;
-			for (int i = 0; i < j; i++) {
-				byte byte0 = md[i];
-				buf[k++] = hexDigits[byte0 >>> 4 & 0xf];
-				buf[k++] = hexDigits[byte0 & 0xf];
-			}
-			return new String(buf);
-		} catch (Exception e) {
-			return null;
-		}
+	//这里是加解密函数抱歉不能显示给大家看到
 	}
 
 	public static String GetDIDIrequestDATA(String p_period_name, String reqid,
